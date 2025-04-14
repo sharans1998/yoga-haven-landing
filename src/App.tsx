e0f952891e3dcb8e940b0014e6e4b84e1087cb0b
Sharan Satish <sharan1998satish@gmail.com>
2025-04-14 19:13:11 +0530

    try fixing app routes.

--- src/App.tsx
+++ src/App.tsx
@@ -26,10 +26,10 @@ const App = () => {
             <Route path="/about" element={<About />} />
             <Route path="/teachers-training" element={<TeachersTraining />} />
             <Route
-              path="/postpartum-fitness"
+              path="postpartum-fitness"
               element={<PostpartumFitnessAd />}
             />
-            <Route path="/postpartum/" element={<PostpartumFitness />} />
+            <Route path="/postpartum" element={<PostpartumFitness />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
